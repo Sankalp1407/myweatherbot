a2720bf49d992bebf8f9ef65e5bd5a5b98cc946f
Sankalp Jaiswal <sankalpjai14@gmail.com>
2026-06-18 13:27:58 +0530

    Version test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'VERSION_999'
 
 # geting and sending response to dialogflow
 @app.route('/webhook', methods=['POST'])
